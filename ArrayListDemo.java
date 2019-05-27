6b74cc136a56385ce73afb9bc7090630e520cfc1
maabDE <49753327+maabDE@users.noreply.github.com>
2019-05-27 16:40:10 +0800

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,15 @@
+package ArrayListTest;
+
+public class ArrayListDemo {
+    private final static long serialVersionUID=8683452581122892189L;//版本号，用于序列化
+    private final static int DEFAULT_CAPACITY=10;//默认长度
+    private final static Object[] EMPTY_ELEMENTDATA={};//数组初始
+    private transient Object[] ELEMENTDATA;//数组底层结构
+    private int size;//数组大小
+    private final static int MAX_ARRAY_SIZE=Integer.MAX_VALUE-8;//最大数组长度(剩余保存头节点)
+
+
+
+
+
+}
